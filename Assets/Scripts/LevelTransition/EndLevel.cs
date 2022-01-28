1437f437b4791c579b7802ba289e3aa439e1d3b0
Stef <theepicstef@users.noreply.github.com>
2022-01-29 11:40:21 +1300

    Created the End Level Trigger
    Added an empty object into sample scene to test.
    Updated the Door script to remove the update function.

---
+++ Assets/Scripts/LevelTransition/EndLevel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour
+{
+    // The Trigger to load the next level
+    public Collider2D endTrigger;
+    // The Level to load
+    public string nextScene;
+
+    void Awake()
+    {
+        endTrigger = GetComponent<Collider2D>();
+    }
+
+    // Will load the next scene
+    // At the moment transition is instant. Added an animation and delay will make it feel nicer.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
