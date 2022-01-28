fdc399ea73c2a64304a4e9d758e922095696f423
jacob sullivan <puremight01@gmail.com>
2022-01-28 22:22:26 +1300

    player freezes in the ice
    when the player is inside the water when it freezes they are frozen inside

--- Assets/Scripts/TransitionScripts/CollisionTransition.cs
+++ Assets/Scripts/TransitionScripts/CollisionTransition.cs
@@ -7,25 +7,38 @@ public class CollisionTransition : MonoBehaviour
     public Collider2D CollisionObject;
     public bool SolidInWinter;
     public WorldTransition CurrentSeason;
+    public List<Collider2D> inside;
+
+    private ContactFilter2D ContactFilter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ContactFilter.SetLayerMask(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         if(SolidInWinter == CurrentSeason.Summer)
         {
+            foreach (Collider2D i in inside)
+            {
+                i.attachedRigidbody.bodyType = RigidbodyType2D.Dynamic;
+            }
             CollisionObject.isTrigger = true;
             gameObject.layer = 0;
+            CollisionObject.OverlapCollider(ContactFilter, inside);
         }
         else
         {
+            foreach(Collider2D i in inside)
+            {
+               i.attachedRigidbody.bodyType=RigidbodyType2D.Static;
+            }
             CollisionObject.isTrigger = false;
             gameObject.layer = 6;
+            
         }
     }
 }
