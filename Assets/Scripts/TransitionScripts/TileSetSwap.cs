10f7b9297be65ffab8c7dc8b07ccb087327ea5e4
jacob sullivan <puremight01@gmail.com>
2022-01-29 13:53:58 +1300

    added tile change on runtime
    had to remake  some of the tiles to get all assets to change across. new music track added to player and tempo updated to match in prefab

--- Assets/Scripts/TransitionScripts/TileSetSwap.cs
+++ Assets/Scripts/TransitionScripts/TileSetSwap.cs
@@ -2,23 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-[System.Serializable]
-public struct TileCouple
-{
-    public TileBase tileA;
-    public TileBase tileB;
-}
-
 
 public class TileSetSwap : MonoBehaviour
 {
-    public List<TileCouple> AllTiles ;
-
+    public TileSet TileCouples ;
     void Start()
     {
+        
         Tilemap tilemap = GetComponent<Tilemap>();
-        foreach (TileCouple i in AllTiles)
+        foreach (TileCouple i in TileCouples.tiles.AllTiles)
         {
+           
             tilemap.SwapTile(i.tileA, i.tileB);
         }
     }
