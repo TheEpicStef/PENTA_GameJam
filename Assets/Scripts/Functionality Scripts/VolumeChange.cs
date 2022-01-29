df6a85f75a9d47bbd1b1c3fba7d608b252492198
Stef <theepicstef@users.noreply.github.com>
2022-01-29 19:15:41 +1300

    Started the volume changing. Will have to set up to pause menu and main menu

---
+++ Assets/Scripts/Functionality Scripts/VolumeChange.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeChange : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public Slider volSlider;
+
+    public void Update()
+    {
+        //float value;
+        //mixer.GetFloat("musicVol", out value);
+        //value = Mathf.Pow(10, (value / 20));
+        //volSlider.value = value;
+        //Debug.Log(value);
+
+        //setSliderValue(volSlider, "musicVol");
+    }
+
+    public void SetMusicLevel(float _sliderValue)
+    {
+        mixer.SetFloat("musicVol", Mathf.Log10(_sliderValue) * 20);
+    }
+
+    public void setSFXLevel(float _sliderValue)
+    {
+        mixer.SetFloat("sfxVol", Mathf.Log10(_sliderValue) * 20);
+    }
+
+    public void setMasterLevel(float _sliderValue)
+    {
+        mixer.SetFloat("masterVol", Mathf.Log10(_sliderValue) * 20);
+    }
+
+    void setSliderValue(Slider _slider, string _name)
+    {
+        float value;
+        mixer.GetFloat(_name, out value);
+        value = Mathf.Pow(10, (value / 20));
+        _slider.value = value;
+        Debug.Log(value);
+    }
+}
